4aaa95a25af04cf0f2faf8b54ec15da41984af89
Lawando69 <lawand15.kong@gmail.com>
2025-01-16 12:10:23 +0000

    Finished completeting the registration section. Input DOB is still not confirmed for either type text and date.

--- src/Forms/AuthForms/RegisterForm.js
+++ src/Forms/AuthForms/RegisterForm.js
@@ -9,6 +9,8 @@ export default function RegisterForm({ toggle }){
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [name, setName] = useState("");
+    const [DOB, setDOB] = useState("");
+    const [health, setHealth] = useState("");
     const [error, setError] = useState("");
 
     const { user, login, logout } = useContext(MainContext);
@@ -20,10 +22,42 @@ export default function RegisterForm({ toggle }){
         const FORM_DATA = {
             "email": email,
             "password": password,
-            "username": name
+            "name": name,
+            "DOB" : DOB,
+            "health" : health
+
         }
 
         axios.post(ENDPOINT, FORM_DATA)
-            
+            .then(response => {
+                console.log(response.data)
+                login(response.data)
+            })
+            .catch(error => {
+                setError(error?.response?.data?.detail || "Error occured")
+            })
     }
+    return(
+        <form className="auth-form" onSubmit={register}>
+            <input type="email" placeholder="Email Address" value={email} onChange={(em => setEmail(em.target.value))} required />
+            <input type="text" placeholder="Full name" value={name} onChange={(nm => setName(nm.target.value))} required />
+            <input type="text" placeholder="DD/MM/YYYY" value={DOB} onChange={(d => setDOB(d.target.value))} required />
+            <input type="password" placeholder="Password" value={password} onChange={(pass => setPassword(pass.target.value))} required/>
+            <input type="text" placeholder="Health detail" value={health} onChange={(hlth => setHealth(hlth.target.value))} />
+            {/*NOTE: Check the input type for the DOB, if text or DOB depending on datetime library*/}
+
+            <div className="auth-options-row">
+                <button type="submit" className="auth-confirm">Register</button>
+
+                <div className="auth-swap-container">
+                    <span>Already have an account?</span>
+                    <button className="auth-swap-btn" onClick={toggle}>Log In</button>
+                </div>
+            </div>
+            {
+                error ? <div className="error">{error}</div>
+                    : null
+            }
+        </form>
+    )
 }
