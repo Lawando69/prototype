bd6ad10287133ff042c48749a1eeb51875e410ed
Lawando69 <lawand15.kong@gmail.com>
2025-01-28 16:30:42 +0000

    Making some changes in the weatherdata

---
+++ src/Components/WeatherData/WeatherData.js
@@ -0,0 +1,70 @@
+import "./WeatherData.css";
+import React, {useState} from "react";
+import axios from "axios";
+
+export default function WeatherData(){
+    const [temperature, setTemperature] = useState("");
+    const [precipition, setPrecipitation] = useState("");
+    const [wind, setWind] = useState("");
+    const [city, setCity] = useState("London");
+    const [error, setError] = useState();
+    const [isLoading, setIsLoading] = useState(true);
+
+    const getWeatherData = (city) => {
+        axios({
+            method: "GET",
+            url: `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=8fad9c886225d5088e4dfea3a18893c0`
+        })
+        .then((response) => {
+            console.log([ response.data.list ]);
+
+            let weeks = []
+            let current_week = []
+
+            for (let i in response.data.list){
+
+                
+
+                if ( (i + 7) % 7 == 0 && i != 0 ){
+                    console.log("pushed on index: ", i)
+                    weeks.push(current_week)
+                    current_week = []
+                }
+
+                current_week.push(response.data.list[i]);
+
+            } 
+
+            if(current_week.length > 0){
+                weeks.push(current_week)
+            }
+
+            // console.log("visible: ")
+            // console.log(weeks[0][0].visibility);
+            //NOTE: The code within the Brackets above allows us to access the [] the first list, [] then the second list
+
+            setIsLoading(false);
+        })
+        .catch((error) => {
+            setError(error?.response?.data?.detail || "Error Occured");
+            setIsLoading(false);
+        }, [] );
+    };
+
+    return(
+        <>
+            <div className="weather-container">
+                <div className="weather-component">
+                    <br/>
+                    {city} Weather
+                    <br/>
+                    {temperature}  
+                </div>
+                <br/>
+                <input type="text" value={city} onChange={(ci => setCity(ci.target.value))} />
+                <button onClick={() => {getWeatherData(city);}}>GET</button>
+            </div>
+        </>
+    )
+}
+
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,23 +1,13 @@
 import "./Home.css"
-import react, {useEffect, useState} from "react";
-import axios from 'axios';
-import Logo from "../../Components/Logo/Logo";
+import WeatherData from "../../Components/WeatherData/WeatherData";
 
 
 export default function Home(){
+    //Add the catch feature
 
-    const [isLoading, setIsLoading] = useState(true);
-    const [error, setError] = useState();
-
-    const weather_api = "https://api.openweathermap.org/data/2.5/forecast?id=524901&appid=8fad9c886225d5088e4dfea3a18893c0"; //NOTE: This will be used to add the api link later.
-    
     return(
-        useEffect(() => {
-            axios.get(weather_api)
-            .then((response) => {
-                
-            })
-        })
-
+        <div className="home-container">
+            <WeatherData />
+        </div>
     )
 }
